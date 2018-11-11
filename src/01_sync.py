bd80c4452e2c13596851178a8ddb17abaeace4e0
bdelate <brendon.d@tangentsolutions.co.za>
2018-11-11 13:45:35 +0200

    Added timing to sync script.

--- src/01_sync.py
+++ src/01_sync.py
@@ -47,10 +47,19 @@ def output_num_seasons(series: str, templates: list) -> None:
             print(f'- {series} > {num_seasons}')
 
 
-def main():
+def main() -> None:
+    io_time, cpu_time = 0.0, 0.0
+    total_time_start = time.time()
     for series in tv_series:
+        io_start = time.time()
         response = get_api_data(series=series)
+        io_time += time.time() - io_start
+        cpu_start = time.time()
         process_response(series=series, response=response)
+        cpu_time += time.time() - cpu_start
+    print(f'\nCPU time: {round(cpu_time, 2)}')
+    print(f'IO time: {round(io_time, 2)}')
+    print(f'Total time: {round(time.time() - total_time_start, 2)}')
 
 
 if __name__ == '__main__':
